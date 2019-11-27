5269538b6409e9ef560f63c6ce3146eb53906c93
Oluwaleke Tade <leke.tade@gmail.com>
2019-11-28 07:34:17 +1000

    Implement User Authentication
    * add and use authRoutes express router to app.js
    * add routes/auth login route
    * set up and export controllers/auth getLogin function
    * add auth login.ejs view
    * adjust navigation.ejs template to display "login" link
    * adjust styling

--- app.js
+++ app.js
@@ -1,69 +1,74 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
 
 // Require mongodb connection from the /util/database folder
 // const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-    User.findById('5dc35cdda33d9b230481b685')
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+  User.findById("5dc35cdda33d9b230481b685")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 // This is the callback function to spin-up the server only after connecting to the DB
 
-mongoose.connect('mongodb+srv://admin-mrtade:Momo123@cluster0-ft6tk.mongodb.net/shopDB?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
-.then((connection) => {
+mongoose
+  .connect(
+    "mongodb+srv://admin-mrtade:Momo123@cluster0-ft6tk.mongodb.net/shopDB?retryWrites=true&w=majority",
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  )
+  .then(connection => {
     // console.log(connection);
-    console.log('*=*=*=*=*=*=*=*=*=*=*=*=*=*=*');
-    console.log('Database connection successful!');
+    console.log("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+    console.log("Database connection successful!");
     User.findOne().then(user => {
-        if (!user) {
-            const user = new User({
-                name: 'Me',
-                email: 'me@test.com',
-                cart: {
-                    items: []
-                }
-            });
-            user.save();
-        }
-        console.log('*=*=*=*=*=*=*=*=*=*=*=*=*=*=*');
-        console.log(`Existing user "${user._id}" is connected!`);
-        console.log('*=*=*=*=*=*=*=*=*=*=*=*=*=*=*');
-
+      if (!user) {
+        const user = new User({
+          name: "Me",
+          email: "me@test.com",
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+      console.log("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+      console.log(`Existing user "${user._id}" is connected!`);
+      console.log("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
     });
 
     app.listen(3000);
-    console.log('*=*=*=*=*=*=*=*=*=*=*=*=*=*=*'); 
-    console.log('Server started on port 3000.');
-})
-.catch((err) => {
+    console.log("*=*=*=*=*=*=*=*=*=*=*=*=*=*=*");
+    console.log("Server started on port 3000.");
+  })
+  .catch(err => {
     console.log(err);
-});
+  });
