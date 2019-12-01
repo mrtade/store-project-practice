0d7ba32a771bd916617a4e48a603d4d45e8476f7
Oluwaleke Tade <leke.tade@gmail.com>
2019-12-01 12:51:55 +1000

    Add Logout functionality
    * Edit views with log-in/log-out logic using isAuthenticated boolean value
    * Add postLogout route
    * Add postLogout controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -65,7 +65,6 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-
   Product.findById(prodId)
     .then(product => {
       return req.session.user.addToCart(product);
